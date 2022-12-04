0b7b5282d1fc6204e983704ec63d39a5669adf02
Facuscar <facuscardillo@gmail.com>
2022-12-04 15:26:42 -0300

    get categories from API

--- context/CategoryProvider.jsx
+++ context/CategoryProvider.jsx
@@ -1,11 +1,30 @@
 import { useState, useEffect, createContext } from "react";
+import axios from "axios";
 
 const CategoryContext = createContext();
 
 export const CategoryProvider = ({children}) => {
+    const [categories, setCategories] = useState([]);
+
+    const getCategories = async () => {
+        try {
+            const url = 'www.thecocktaildb.com/api/json/v1/1/list.php?c=list'
+
+            const { data } = await axios(url)
+            setCategories(data.drinks);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    useEffect(() => {
+        getCategories();
+    }, []);
 
     return (
-        <CategoryContext.Provider value={{}}>
+        <CategoryContext.Provider value={{
+            categories,
+        }}>
             {children}
         </CategoryContext.Provider>
     )
