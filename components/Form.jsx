e171f4b77f21403c3cfbe9cce6fdf528e8fd3004
Facuscar <facuscardillo@gmail.com>
2022-12-05 23:27:32 -0300

    Get values after submit

--- components/Form.jsx
+++ components/Form.jsx
@@ -1,23 +1,48 @@
-import { Button, Form, Row, Col } from 'react-bootstrap';
+import { useRef, useState } from 'react';
+import { Button, Form, Row, Col, Alert } from 'react-bootstrap';
 import useCategories from '../hooks/useCategories';
 
 const DrinksForm = () => {
 
+    const nameRef = useRef();
+    const categoryRef = useRef();
+
+    const [alert, setAlert] = useState('');
     const { categories } = useCategories();
 
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        const search = {
+            name: nameRef.current.value,
+            categories: categoryRef.current.value,
+        }
+
+        if(Object.values(search).includes('')) {
+            setAlert('All fields are required');
+            return
+        }
+
+        setTimeout(() => {
+            setAlert('');
+        }, 3000);
+
+    }
+
     return (
-        <Form>
+        <Form onSubmit={handleSubmit} >
+            { alert && <Alert variant='danger'>{alert}</Alert> }
             <Row>
                 <Col md={6}>
                     <Form.Group>
                         <Form.Label htmlFor='name'>Drink name</Form.Label>
-                        <Form.Control type='text' placeholder='Tequila' name='name' id='name' />
+                        <Form.Control type='text' placeholder='Tequila' name='name' id='name' ref={nameRef} />
                     </Form.Group>
                 </Col>
                 <Col md={6}>
                 <Form.Group>
                         <Form.Label htmlFor='name'>Category name</Form.Label>
-                        <Form.Select id='category' name='category'>
+                        <Form.Select id='category' name='category' ref={categoryRef}>
                             <option value="">- Choose a category -</option>
                             { categories && categories.map(category => (
                                 <option key={category.strCategory} value={category.strCategory}>{category.strCategory}</option>
@@ -28,7 +53,7 @@ const DrinksForm = () => {
             </Row>
             <Row className='justify-content-end mt-4'>
                 <Col md={3}>
-                    <Button variant='danger' className='text-uppercase w-100'>
+                    <Button variant='danger' className='text-uppercase w-100' type='submit' >
                         Search Drinks
                     </Button>
                 </Col>
