71f00106e0f3fd6391975c44647b56b7337ab01b
Facuscar <facuscardillo@gmail.com>
2022-12-07 19:25:42 -0300

    Small styles fixes

--- components/Drink.jsx
+++ components/Drink.jsx
@@ -5,11 +5,11 @@ const Drink = ({drink}) => {
     const { handleModalClick } = useDrinks();
 
     return (
-        <Col md={2} lg={3}>
+        <Col md={2} lg={3} className='mb-5'>
             <Card className='mb-1'>
                 <Card.Img variant='top' src={drink.strDrinkThumb} alt={drink.strDrink} />
             </Card>
-            <Card.Body>
+            <Card.Body className='d-flex flex-column justify-content-between' style={{height: "135px"}}>
                 <Card.Title>{drink.strDrink}</Card.Title>
                 <Button className='w-100 text-uppercase mt-2' variant='danger' onClick={() => handleModalClick()}>
                     See recipe
