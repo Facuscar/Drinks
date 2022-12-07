1e53d70f5644628eacc90c2bb1b9c6b1204edb5b
Facuscar <facuscardillo@gmail.com>
2022-12-06 21:29:31 -0300

    Styles for each drink

--- components/Drink.jsx
+++ components/Drink.jsx
@@ -1,6 +1,18 @@
+import { Col, Card, Button } from 'react-bootstrap';
+
 const Drink = ({drink}) => {
     return (
-        <h1>{drink.strDrink}</h1>
+        <Col md={2} lg={3}>
+            <Card className='mb-1'>
+                <Card.Img variant='top' src={drink.strDrinkThumb} alt={drink.strDrink} />
+            </Card>
+            <Card.Body>
+                <Card.Title>{drink.strDrink}</Card.Title>
+                <Button className='w-100 text-uppercase mt-2' variant='danger'>
+                    See recipe
+                </Button>
+            </Card.Body>
+        </Col>
     );
 }
 
